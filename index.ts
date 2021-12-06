70aae87cbd2cecffe04b571c1559a2c617c613ae
igor.pieruccini <igorpieruccini@gmail.com>
2021-12-06 20:34:47 +0100

    remove unnecessary files from root

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from "./types";
-import R from "ramda";
-
-// filter complete task
-// const completeTasks = R.filter<Task>((val) => val.complete);
-const completeTasks = R.filter<Task>(R.where({ complete: true }));
-
-// filter by author
--- types.ts
+++
@@ -1,6 +0,0 @@
-export interface Task {
-  author: string;
-  dueData: number;
-  title: string;
-  complete: boolean;
-}
